de8a343cdabf8acfbd512334c08632583807d371
brilliahib <brilliahib21@gmail.com>
2024-02-07 13:58:09 +0700

    fix route

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./components/Pages/home.jsx";
 import SalokaPage from "./components/Pages/CardDetailPage/saloka.jsx";
 import GedongSongoPage from "./components/Pages/CardDetailPage/gedongsongo.jsx";
@@ -10,35 +9,20 @@ import KotaLamaPage from "./components/Pages/CardDetailPage/kotalama.jsx";
 import SampokongPage from "./components/Pages/CardDetailPage/sampokong.jsx";
 import DestinationPage from "./components/Pages/destination.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage></HomePage>,
-  },
-  {
-    path: "/destination",
-    element: <DestinationPage></DestinationPage>,
-  },
-  {
-    path: "/get-to-know/saloka-park",
-    element: <SalokaPage></SalokaPage>,
-  },
-  {
-    path: "/get-to-know/candi-gedong-songo",
-    element: <GedongSongoPage />,
-  },
-  {
-    path: "/get-to-know/kota-lama",
-    element: <KotaLamaPage />,
-  },
-  {
-    path: "/get-to-know/sampokong",
-    element: <SampokongPage />,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/destination" element={<DestinationPage />} />
+        <Route path="/get-to-know/saloka-park" element={<SalokaPage />} />
+        <Route
+          path="/get-to-know/candi-gedong-songo"
+          element={<GedongSongoPage />}
+        />
+        <Route path="/get-to-know/kota-lama" element={<KotaLamaPage />} />
+        <Route path="/get-to-know/sampokong" element={<SampokongPage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
