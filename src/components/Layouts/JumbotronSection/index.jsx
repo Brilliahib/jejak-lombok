7ab1a292f2304dcfbc5e216e0d6ae1c965f42a45
brilliahib <brilliahib21@gmail.com>
2024-03-02 23:58:52 +0700

    add text shadow on jumbotron

--- src/components/Layouts/JumbotronSection/index.jsx
+++ src/components/Layouts/JumbotronSection/index.jsx
@@ -12,7 +12,7 @@ const Jumbotron = () => {
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 pt-16 jumbotron">
         <div className="flex items-center h-[100vh] justify-center text-center">
           <h1
-            className="text-6xl font-black leading-tight mb-8 tracking-wide text-white"
+            className="text-6xl font-black leading-tight mb-8 tracking-wide text-white text-shadow"
             data-aos="fade-right"
             data-aos-once="false"
             data-aos-duration="1000"
